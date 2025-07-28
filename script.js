9debaa5ba6659786caf601967f86dea903c5fda4
spookie720 <cosimaoctavia720@gmail.com>
2025-07-28 10:00:28 -0600

    Game State Basics

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 alert("Welcome to 'They Filed the Skin Away'");
 console.log("Welcome to 'They Filed the Skin Away'");
+// Game State holds everything that changes druring play
+const gameState = {
+    currentScene: 'observationCell',
+    inventory: [],
+    petTrust: 5, // trust scale 0 = hostile, 10 = bonded
+    logsCollected: [],
+    choices:{},
+    flags:{
+        hasAccessCard: false,
+        unlockedRoom: false,
+        lifeSupportdisabled: false,
+        helpedCreatureInTank: false,
+    }
+};
